bc118c4500333b6729dec37ddc083781947415c1
cagdasdoner <donercagdas@gmail.com>
2017-05-21 16:55:42 +0300

    Indentation please.

--- src/device/raspberry/camera.c
+++ src/device/raspberry/camera.c
@@ -14,26 +14,26 @@ void* CAMThread()
     char system_cmd[MAX_SYSTEM_CMD_LEN];
     memset(system_cmd, 0, MAX_SYSTEM_CMD_LEN);
     sprintf(system_cmd, "raspistill -o %s", CAM_FILE);
-	system(system_cmd);
+    system(system_cmd);
 
-	if( access( CAM_FILE, F_OK ) != -1 )
-	{
-    	printf("\nFile found, mailing...\n");
+    if( access( CAM_FILE, F_OK ) != -1 )
+    {
+        printf("\nFile found, mailing...\n");
         memset(system_cmd, 0, MAX_SYSTEM_CMD_LEN);
         sprintf(system_cmd, "mpack -s \"Captured\" %s %s", CAM_FILE, EMAIL_TO_POST);
-    	system(system_cmd);
-    	printf("\nMail sent.\n");
+        system(system_cmd);
+        printf("\nMail sent.\n");
         memset(system_cmd, 0, MAX_SYSTEM_CMD_LEN);
         sprintf(system_cmd, "rm -rf %s", CAM_FILE);
-    	system(system_cmd);
-    	printf("Cleared. OK.\n");
-	}
-	else
-	{
-		printf("File NOT found!!!!!!\n");
-	}
+        system(system_cmd);
+        printf("Cleared. OK.\n");
+    }
+    else
+    {
+        printf("File NOT found!!!!!!\n");
+    }
     pthread_cancel(tid);
-	printf("\nThread was deleted successfully.\n");
+    printf("\nThread was deleted successfully.\n");
     
     return NULL;
 }
